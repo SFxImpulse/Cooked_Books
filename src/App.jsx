16a5c6f1352caaca820f295c3993795d5420999c
David Braverman <braverman.david.business@gmail.com>
2022-10-19 16:35:41 -0400

    Separate axios call into hook.

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,17 @@ import About from './components/About';
 import RecipeList from "./components/RecipeList";
 import GroceryList from './components/GroceryList';
 import FavouritesList from './components/FavouritesList';
-import { useState, useEffect } from "react";
-import axios from "axios";
+
+import useFetchedData from './hooks/useFetchedData';
 
 function App() {
 
-  const [state, setState] = useState({
-    nav: false,
-    mode: 0,
-    recipe: false,
-    recipies: [],
-    favourites: [],
-    ingredients: [],
-    groceryList: [],
-  });
-
-  const setNav = (nav) => setState((prev) => ({ ...prev, nav }));
-  const setMode = (mode) => setState((prev) => ({ ...prev, mode }));
-  const setRecipe = (recipe) => setState((prev) => ({ ...prev, recipe }));
-  // const setFavourites = (favourites) => setState((prev) => ({ ...prev, favourites }));
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/recipies"),
-      axios.get("/api/ingredients"),
-      axios.get("/api/grocery_list"),
-    ]).then((all) => {
-      console.log(all);
-      setState((prev) => ({
-        ...prev,
-        recipies: all[0].data,
-        ingredients: all[1].data,
-        groceryList: all[2].data,
-      }));
-      console.log(state);
-    });
-  }, []);
-
-  const getRecipies = (state, recipe) => {
-
-    const output = [];
-
-    const recipeObj = state.recipies.find(r => r.name === recipe)
-
-    // console.log(recipeObj);
-
-    if (!recipeObj) {
-      return [];
-    }
-  }
-
-  // console.log(getRecipies(state, "Pancakes"));
+  const {
+    state,
+    setNav,
+    setMode,
+    setRecipe
+  } = useFetchedData();
 
   return (
     <div>
---
+++ src/hooks/useFetchedData.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useFetchedData() {
+
+  const [state, setState] = useState({
+    nav: false,
+    mode: 0,
+    recipe: false,
+    recipies: [],
+    favourites: [],
+    ingredients: [],
+    groceryList: [],
+  });
+
+  const setNav = (nav) => setState((prev) => ({ ...prev, nav }));
+  const setMode = (mode) => setState((prev) => ({ ...prev, mode }));
+  const setRecipe = (recipe) => setState((prev) => ({ ...prev, recipe }));
+  // const setFavourites = (favourites) => setState((prev) => ({ ...prev, favourites }));
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/recipies"),
+      axios.get("/api/ingredients"),
+      axios.get("/api/grocery_list"),
+    ]).then((all) => {
+      setState(prev => ({
+        ...prev,
+        recipies: all[0].data,
+        ingredients: all[1].data,
+        groceryList: all[2].data,
+      }));
+    });
+  }, []);
+
+  console.log(state);
+
+  return { state, setNav, setMode, setRecipe };
+
+};
+
+export default useFetchedData;
